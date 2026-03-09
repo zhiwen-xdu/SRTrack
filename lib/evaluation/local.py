281ce4366e8fc2023f422e6a4a9ac243a0a19908
Zhiwen Chen <50076054+zhiwen-xdu@users.noreply.github.com>
2026-03-09 20:42:23 +0800

    Add files via upload

---
+++ lib/evaluation/local.py
@@ -0,0 +1,6 @@
+class EnvironmentSettings:
+    def __init__(self):
+        self.project_dir = '/home/czwos/Project/SIAMTrack/'
+        self.save_dir = '/home/czwos/Project/SIAMTrack/output/'               # Directory for checkpoint files.
+        self.results_path = '/home/czwos/Project/SIAMTrack/output/test/tracking_results'
+
