1a7f989e58eaeab3ab19c7517950d5d87c7ac5f6
Zhiwen Chen <50076054+zhiwen-xdu@users.noreply.github.com>
2026-03-09 20:43:10 +0800

    Delete lib/evaluation directory

--- lib/evaluation/local.py
+++
@@ -1,6 +0,0 @@
-class EnvironmentSettings:
-    def __init__(self):
-        self.project_dir = '/home/czwos/Project/SIAMTrack/'
-        self.save_dir = '/home/czwos/Project/SIAMTrack/output/'               # Directory for checkpoint files.
-        self.results_path = '/home/czwos/Project/SIAMTrack/output/test/tracking_results'
-
